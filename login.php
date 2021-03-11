403e8726b40d7de986dfe5df111fab8e126997c2
myin3 <60764920+myin3@users.noreply.github.com>
2021-03-11 18:08:10 -0500

    added login

--- login.php
+++ login.php
@@ -11,19 +11,14 @@
 
 	session_start();
 
-	print_r($_SESSION['login']);
+	$_SESSION['login'] = array('admin' => 'admin', 'user' => 'user');
 
 	if(isset($_POST["submit"])){
 
-		$_SESSION['login'] = array('admin' => 'admin', 'user' => 'user');
+		$_SESSION['Username'] = isset($_POST['Username']) ? $_POST["Username"] : '';
+		$_SESSION['Password'] = isset($_POST['Password']) ? $_POST['Password'] : '';
 
-
-
-		
-		$Username = isset($_POST['Username']) ? $_POST["Username"] : '';
-		$Password = isset($_POST['Password']) ? $_POST['Password'] : '';
-
-		if (isset($_SESSION['Username']) && isset($_SESSION['login'][$_SESSION['Username']])){
+		if (isset($_SESSION['Username']) && isset($_SESSION['Password'])){
 			$_SESSION['Points'] = 0;
 			header("location: jeopardyMain.php");
 			exit;
