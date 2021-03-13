a485f5880141c0f3cd828b953189cb09de169828
Neon Bui <47070193+neonbui10@users.noreply.github.com>
2021-03-13 15:46:43 -0500

    Added questions and answers

---
+++ cell14.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Javascript For 300 Points</title>
+	<link rel="stylesheet" type="text/css" href="jeopardystyle.css">
+</head>
+<body>
+	<div class="question"> 
+		What is used to declare a variable?
+	</div>
+	<form action="jeopardyMain.php" method="post">
+		<input class="center-block" type="text" name="answer14">
+		<input class="center-block" type="submit" name="submit">
+	</form>
+</body>
+</html>
---
+++ cell9.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Javascript For 200 Points</title>
+	<link rel="stylesheet" type="text/css" href="jeopardystyle.css">
+</head>
+<body>
+	<div class="question"> 
+		What year was Javascript first created?
+	</div>
+	<form action="jeopardyMain.php" method="post">
+		<input class="center-block" type="text" name="answer9">
+		<input class="center-block" type="submit" name="submit">
+	</form>
+</body>
+</html>
